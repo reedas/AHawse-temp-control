74a1cdfc92ee391c5d8646076044e6da7e276d6c
iotexpert <engineer@iotexpert.com>
2019-12-07 19:15:30 -0500

    simplified main.cpp

--- main.cpp
+++ main.cpp
@@ -11,28 +11,7 @@ Thread temperatureThreadHandle;
 Thread capsenseThreadHandle;
 Thread ntpThreadHandle;
 
-Semaphore WiFiSemaphore;
-
 WiFiInterface *wifi;
-
-// wifiStatusCallback
-// Changes the display when the wifi status is changed
-void wifiStatusCallback(nsapi_event_t status, intptr_t param)
-{
-    switch(param) {
-        case NSAPI_STATUS_LOCAL_UP:
-        break;
-        case NSAPI_STATUS_GLOBAL_UP:
-        break;
-        case NSAPI_STATUS_DISCONNECTED:
-        WiFiSemaphore.release();
-        break;
-        case NSAPI_STATUS_CONNECTING:
-        break;
-        default:
-        break;
-    }
-}
 
 int main()
 {
@@ -41,25 +20,19 @@ int main()
 
     int ret;
     wifi = WiFiInterface::get_default_instance();
-    wifi->attach(&wifiStatusCallback);
-    while(1)
-    {
-        do {
+
+    do {
             ret = wifi->connect("CYFI_IOT_EXT", "cypresswicedwifi101", NSAPI_SECURITY_WPA_WPA2);
             if (ret != 0) {
             ThisThread::sleep_for(2000); // If for some reason it doesnt work wait 2s and try again
             }
-        } while(ret !=0);
+    } while(ret !=0);
 
-        if(blinkThreadHandle.get_state() == Thread::Deleted)
-        {
 
-            ntpThreadHandle.start(ntpThread);
-            blinkThreadHandle.start(blinkThread);
-            displayThreadHandle.start(displayThread);
-            temperatureThreadHandle.start(temperatureThread);
-            capsenseThreadHandle.start(capsenseThread);
-        }
-        WiFiSemaphore.acquire();
-    }
+    ntpThreadHandle.start(ntpThread);
+    blinkThreadHandle.start(blinkThread);
+    displayThreadHandle.start(displayThread);
+    temperatureThreadHandle.start(temperatureThread);
+    capsenseThreadHandle.start(capsenseThread);
+    
 }
